c44272b8c2aecb81782dba85833e1a9fc3678913
Dostal-Filip <email@dostal-filip.cz>
2024-08-08 20:06:42 +0200

    First draft of recipe list
    Created first draft of the recipe list look

---
+++ client/src/bricks/Recipe.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import styles from "../css/recipe.module.css";
+
+function Recipe(props) {
+  return (
+    <Card className={styles.main}>
+      <Card.Body className={styles.mainb}>
+        <div className={styles.icon}>
+        
+       
+ 
+      
+          <img className={styles.image} src={props.recipe.imgUri} ></img>
+          <div className={styles.title}>
+
+          {props.recipe.name} 
+        </div>
+          </div>
+
+    
+        <div className={styles.text}>
+        {props.recipe.description}
+        </div>
+
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Recipe;
