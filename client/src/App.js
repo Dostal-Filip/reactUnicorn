1b78cff00b437c47b13ca68a89c672b688d64fac
Dostal-Filip <email@dostal-filip.cz>
2024-08-15 19:29:33 +0200

    Added server call
    Added server call to get recipe data instead of loading them from mock json

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,57 @@ import "./App.css";
 
 import RecipeList from "./bricks/RecipeList";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from "react";
+import { useState } from "react";
+import Icon from "@mdi/react";
+import { mdiLoading } from "@mdi/js";
 
-
-
-
-let recipes = require('./data/recipes.json');
 
 function App() {
-  return (
-    <div className="App">
-      <RecipeList recipeList={recipes} />
-    </div>
-  );
+  const [recipeLoadCall, setRecipeLoadCall] = useState({
+  state: "pending",
+});
+  useEffect(() => {
+    fetch(`http://localhost:3000//recipe/list`, {
+      method: "GET",
+    }).then(async (response) => {
+      const responseJson = await response.json();
+      if (response.status >= 400) {
+        setRecipeLoadCall({ state: "error", error: responseJson });
+      } else {
+        setRecipeLoadCall({ state: "success", data: responseJson });
+      }
+    });
+  }, []);
+
+  function getChild() {
+    switch (recipeLoadCall.state) {
+      case "pending":
+        return (
+          <div className={"loading"}>
+            <Icon size={2} path={mdiLoading} spin={true} />
+          </div>
+        );
+      case "success":
+        return (
+          <div className="App">
+            <RecipeList recipeList={recipeLoadCall.data} />
+          </div>
+        );
+      case "error":
+        return (
+          <div className={"err"}>
+            <div>Nepodařilo se načíst data o receptech.</div>
+            <br />
+            <pre>{JSON.stringify(recipeLoadCall.error, null, 2)}</pre>
+          </div>
+        );
+      default:
+        return null;
+    }
+  }
+
+  return <div className="App">{getChild()}</div>;
 }
 
 export default App;
